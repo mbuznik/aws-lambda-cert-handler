1ac0a982095f79de263371ff796b51482ca03e1d
Simona Buznik <sbuznik@vmware.com>
2025-02-02 21:26:35 +0200

    refactoring of handler class

--- src/index.ts
+++ src/index.ts
@@ -5,46 +5,53 @@ import { DynamoService } from "./dynamoService.js";
 import { S3Service } from "./s3Service.js";
 dotenv.config();
 
+export class LambdaHandler {
+	private dynamoService: DynamoService;
+	private s3Service: S3Service;
+	private certificateService: CertificateService;
 
-export const handler = async (event: any) => {
-	try {
-		const dynamoService = new DynamoService();
-		const s3Service = new S3Service();
-		const certificateService = new CertificateService();
-		const bucketName = process.env.BUCKET_NAME || AWSConstants.BUCKET_NAME;
-		const key = process.env.CERTIFICATE_KEY || AWSConstants.CERTIFICATE_KEY;
-		const signingAlgorithm = process.env.SIGNING_ALGORITHM || CertConstants.DEFAULT_SIGNING_ALGORITHM;
-
-		// Fetch certificate from S3
-		const certPem = await s3Service.getCertificateFromS3(bucketName, key);
-		// Extract public key and common name
-		const publicKey = certificateService.extractPublicKey(certPem);
-		const commonName = certificateService.extractCommonName(certPem);
-
-		let signedKey: string;
-		//Choose between ECC and RSA - only used for testing to showcase both
-		if (signingAlgorithm === CertConstants.ECC_ALGORITHM) {
-			const { privateKey: eccPrivateKey } = await certificateService.generateECCKeyPair();
-			signedKey = await certificateService.signData(publicKey, eccPrivateKey, signingAlgorithm);
-		} else {
-			const { privateKey: rsaPrivateKey } = certificateService.generateRSAKeyPair();
-			signedKey = await certificateService.signData(publicKey, rsaPrivateKey, signingAlgorithm);
-		}
-
-		await dynamoService.storeSignedPublicKey(commonName, signedKey);
-		return {
-			statusCode: 200,
-			body: JSON.stringify("Signed public key stored successfully.")
-		};
-	} catch (err) {
-		console.error("Error:", err);
-		return {
-			statusCode: 500,
-			body: JSON.stringify("Error processing the certificate.")
-		};
+	constructor() {
+		this.dynamoService = new DynamoService();
+		this.s3Service = new S3Service();
+		this.certificateService = new CertificateService();
 	}
-};
+	public async handle(event: any) {
+		try {
+			const bucketName = process.env.BUCKET_NAME || AWSConstants.BUCKET_NAME;
+			const key = process.env.CERTIFICATE_KEY || AWSConstants.CERTIFICATE_KEY;
+			const signingAlgorithm = process.env.SIGNING_ALGORITHM || CertConstants.DEFAULT_SIGNING_ALGORITHM;
 
+			// Fetch certificate from S3
+			const certPem = await this.s3Service.getCertificateFromS3(bucketName, key);
+			// Extract public key and common name
+			const publicKey = this.certificateService.extractPublicKey(certPem);
+			const commonName = this.certificateService.extractCommonName(certPem);
+
+			let signedKey: string;
+			//Choose between ECC and RSA - only used for testing to showcase both
+			if (signingAlgorithm === CertConstants.ECC_ALGORITHM) {
+				const { privateKey: eccPrivateKey } = await this.certificateService.generateECCKeyPair();
+				signedKey = await this.certificateService.signData(publicKey, eccPrivateKey, signingAlgorithm);
+			} else {
+				const { privateKey: rsaPrivateKey } = this.certificateService.generateRSAKeyPair();
+				signedKey = await this.certificateService.signData(publicKey, rsaPrivateKey, signingAlgorithm);
+			}
+
+			await this.dynamoService.storeSignedPublicKey(commonName, signedKey);
+			return {
+				statusCode: 200,
+				body: JSON.stringify("Signed public key stored successfully.")
+			};
+		} catch (err) {
+			console.error("Error:", err);
+			return {
+				statusCode: 500,
+				body: JSON.stringify("Error processing the certificate.")
+			};
+		}
+	}
+
+}
 // Using this for local testing, will be removed for prod #TODO
 (async () => {
 	console.log("Running handler...");
@@ -59,7 +66,8 @@ export const handler = async (event: any) => {
 		]
 	};
 	try {
-		const result = await handler(mockEvent);
+		const lambdaHandler = new LambdaHandler();
+		const result = await lambdaHandler.handle(mockEvent);
 		console.log("Handler executed successfully:", result);
 	} catch (error) {
 		console.error("Handler execution failed:", error);
